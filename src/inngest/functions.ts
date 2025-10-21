777d09d818c0db91110ff711393cbb3af82769ff
LDI <debibidon@gmail.com>
2025-10-21 19:25:07 +0300

    08: error tracking

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,6 +1,7 @@
 import { inngest } from "./client";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { generateText } from "ai";
+import * as Sentry from "@sentry/nextjs";
 
 const google = createGoogleGenerativeAI();
 
@@ -8,6 +9,10 @@ export const execute = inngest.createFunction(
   { id: "execute" },
   { event: "execute/ai" },
   async ({ event, step }) => {
+    console.warn("Executing AI function");
+    console.error("The error I want to track");
+    Sentry.logger.info('User triggered test log', { log_source: 'sentry_test' });
+
     const { steps } = await step.ai.wrap(
       "gemini-generate-text",
       generateText,
@@ -15,6 +20,11 @@ export const execute = inngest.createFunction(
         model: google("gemini-2.5-flash"),
         system: "You are a helpful assistant.",
         prompt: "What is 2 + 2?",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
       }
     );
 
